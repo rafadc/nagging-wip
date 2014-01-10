3227c3a9e98cc32a1cbae9f82cfb741cf2bc8fd1
rdecastro <rdecastro@avalonbiometrics.com>
2014-01-10 15:01:00 +0100

    Added another test to really check if the wip was increased

--- spec/wip_spec.rb
+++ spec/wip_spec.rb
@@ -1,8 +1,22 @@
-def nagging_wip
+def nagging_wip(notifier, project, max_wip)
 end
 
 describe "nagging wip notifier" do 
 	it "does not send a notification if WIP over the specified value" do
-    nagging_wip
+    notifier = double("Notifier")
+    project = double("Project")
+    max_wip = 4
+    project.stub(:curent_wip).and_return(2)
+    nagging_wip(notifier, project, max_wip)
+    notifier.should_not_receive(:wip_exceeded)
+  end
+
+  it "does send a notification if WIP over the specified value" do
+    notifier = double("Notifier")
+    project = double("Project")
+    max_wip = 4
+    project.stub(:curent_wip).and_return(7)
+    nagging_wip(notifier, project, max_wip)
+    notifier.should_receive(:wip_exceeded)
   end
 end
