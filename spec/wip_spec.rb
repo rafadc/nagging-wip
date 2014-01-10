1679c38cff28f75b35bf7e0cc61e4e12587a8073
rdecastro <rdecastro@avalonbiometrics.com>
2014-01-10 15:09:34 +0100

    Now we notify when needed

--- spec/wip_spec.rb
+++ spec/wip_spec.rb
@@ -1,22 +1,22 @@
 def nagging_wip(notifier, project, max_wip)
+  notifier.wip_exceeded if project.current_wip > max_wip
 end
 
 describe "nagging wip notifier" do 
-	it "does not send a notification if WIP over the specified value" do
-    notifier = double("Notifier")
-    project = double("Project")
-    max_wip = 4
-    project.stub(:curent_wip).and_return(2)
-    nagging_wip(notifier, project, max_wip)
-    notifier.should_not_receive(:wip_exceeded)
+  before :each do
+    @notifier = double("Notifier")
+    @max_wip = 4
+  end
+    
+  it "does not send a notification if WIP over the specified value" do
+    project = double("Project", :current_wip => 4)
+    @notifier.should_not_receive(:wip_exceeded)
+    nagging_wip(@notifier, project, @max_wip)
   end
 
   it "does send a notification if WIP over the specified value" do
-    notifier = double("Notifier")
-    project = double("Project")
-    max_wip = 4
-    project.stub(:curent_wip).and_return(7)
-    nagging_wip(notifier, project, max_wip)
-    notifier.should_receive(:wip_exceeded)
+    project = double("Project", :current_wip => 7)
+    @notifier.should_receive(:wip_exceeded)
+    nagging_wip(@notifier, project, @max_wip)
   end
 end
