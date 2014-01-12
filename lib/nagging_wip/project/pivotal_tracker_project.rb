469dc73ab47e4f400f9b57aabdd14e51feb571cb
Rafa de Castro <rafael@micubiculo.com>
2014-01-12 10:48:15 +0100

    Fixed problem with injection of dependency

--- lib/nagging_wip/project/pivotal_tracker_project.rb
+++ lib/nagging_wip/project/pivotal_tracker_project.rb
@@ -2,12 +2,12 @@ class PivotalTrackerProject
   attr_writer :pivotal_api
 
   def initialize(config)
-    project_config = config.for(:pivotal_tracker_project)
-    @max_wip = project_config['max_wip']
+    @project_config = config.for(:pivotal_tracker_project)
+    @max_wip = @project_config['max_wip']
   end
 
   def current_wip
-    @pivotal_api ||= PivotalAPIv5CurrentIterationReader.new(project_config['token'], project_config['project_id'])
+    @pivotal_api ||= PivotalAPIv5CurrentIterationReader.new(@project_config['token'], @project_config['project_id'])
     current = @pivotal_api.current_iteration
     current['stories'].select {|story| story['current_state']=='started' }.size
   end
